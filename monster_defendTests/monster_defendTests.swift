b96cab137f96b918dbdcee4df265bc9a978bceaa
hoanglm6201 <hoanglm@infinitytech.global>
2025-07-16 17:40:55 +0700

    Initial Commit

---
+++ monster_defendTests/monster_defendTests.swift
@@ -0,0 +1,17 @@
+//
+//  monster_defendTests.swift
+//  monster_defendTests
+//
+//  Created by flutter on 16/7/25.
+//
+
+import Testing
+@testable import monster_defend
+
+struct monster_defendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
